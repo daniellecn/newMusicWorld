fb0538795a258e214b9a06bbe7e3183a3d2c2b95
Danielle <ddaniellecn@gmail.com>
2017-09-11 17:15:56 +0300

    merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,24 +15,15 @@ import { Top10ArtistsComponent } from './Components/top10artists.component';
 import { Top10SongsComponent } from './Components/top10songs.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'allSongs', component: SongsComponent},
-=======
-  { path: '', redirectTo: '', pathMatch: 'full', component: VideoComponent },
-  { path: 'allSongs/:action', component: SongsComponent},
->>>>>>> 8bde88fc4fba42ed37b23cee099dcd7e88c84dce
   { path: 'login', component: LoginComponent},
   { path: 'logout', component: LogoutComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'artist', component: ArtistComponent},
   { path: 'artistDetail', component: ArtistDetailComponent},
   { path: 'top10artists', component: Top10ArtistsComponent},
-<<<<<<< HEAD
   { path: 'top10songs', component: SongsComponent}
-=======
-  { path: 'top10songs', component: Top10SongsComponent}
->>>>>>> 8bde88fc4fba42ed37b23cee099dcd7e88c84dce
 ];
 
 @NgModule({
