ffbba39207cbdbaf4962147300f313f49461620c
Daniel Jakobsen <daniel@any-app.com>
2017-09-12 00:46:49 +0300

    fix styles

--- src/app/Mocks/mock-artists.ts
+++
@@ -1,6 +0,0 @@
-import { Artist } from './../Modules/artist';
-
-export const ARTISTS: Artist[] = [
-  { id: 1, firstName: 'Shlomo', lastName: 'Artzi', country: 'Israel', views: 2 },
-  { id: 2, firstName: 'Britney', lastName: 'Spears', country: 'Mississippi' },
-];
--- src/app/Mocks/mock-users.ts
+++
@@ -1,6 +0,0 @@
-import { User } from './../Modules/user';
-
-export const USERS: User[] = [
-  { id: '1', userName: '1', firstName: "a", lastName: "a", password: '1' },
-  { id: '2', userName: '2', firstName: "b", lastName: "b", password: '2' },
-];
